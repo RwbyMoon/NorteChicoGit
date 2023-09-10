1932a3cc1fb670d49bf00aa6d9da407c733fe502
Benjamin Andula <andula.benjamin@bbox.fr>
2023-09-10 14:25:03 +0200

    bit of porting from template to solution

---
+++ NorteChicoSolution/NorteChicoProject/Core/Civilization_Colors.sql
@@ -0,0 +1,23 @@
+/*
+	Civilization Badge Colours
+	Authors: MC
+*/
+
+-----------------------------------------------
+-- Colors
+
+-- This configures four colour options for your custom civilization. All values are formatted as RGBA - with the A (Alpha) channel always set to 255.
+
+-- In this file/section, all we are doing is defining the colours and giving them unique string 'names'. We'll assign them to our civilization (via our Leader) in a separate file (Leader_Colors.sql).
+-----------------------------------------------
+
+INSERT INTO	Colors
+		(Type,								Color				)
+VALUES	('COLOR_RWB_NORTECHICO_A_PRIMARY',		'0,80,82,255'		),
+		('COLOR_RWB_NORTECHICO_A_SECONDARY',		'187,193,129,255'	),
+		('COLOR_RWB_NORTECHICO_B_PRIMARY',		'187,193,129,255'	),
+		('COLOR_RWB_NORTECHICO_B_SECONDARY',		'0,80,82,255'		),
+		('COLOR_RWB_NORTECHICO_C_PRIMARY',		'255,255,255,255'	),
+		('COLOR_RWB_NORTECHICO_C_SECONDARY',		'0,80,82,255'		),
+		('COLOR_RWB_NORTECHICO_D_PRIMARY',		'0,80,82,255'		),
+		('COLOR_RWB_NORTECHICO_D_SECONDARY',		'255,255,255,255'	);
---
+++ NorteChicoSolution/NorteChicoProject/Leader/Leader_Core.sql
@@ -0,0 +1,84 @@
+/*
+	Core Leader Definition
+	Author: MC
+*/
+
+-----------------------------------------------
+-- Types
+
+-- This inserts the leader reference into the primary Data Types table as a playable Leader. This is mandatory. The string listed under 'Type' must be used throughout the mod when referring to the LeaderType.
+
+-- It is customary for this to take the following format: LEADER_PREFIX_LEADERNAME
+
+-- PREFIX: A 'unique', brief string of characters - often derived from the modder's initials, name or other alias (e.g. MC, MATT or MYALIAS). This is used to differentiate from other mods, primarily.
+-- LEADERNAME: The name of the leader themselves (e.g. CLEOPATRA, GANDHI or VICTORIA).
+-----------------------------------------------
+
+INSERT INTO	Types
+			(Type,					Kind			)
+VALUES		('LEADER_MC_PO_NGBE',	'KIND_LEADER'	);
+		
+-----------------------------------------------
+-- Leaders
+
+-- This defines the LeaderType itself, such that the leader exists as an entity alongside the other leaders.
+
+-- LeaderType: Must match the string defined above for Type.
+-- Name: This locally-defined value appears in Leader_Localisation.sql and will be used as the display string for the leader's name.
+-- InheritFrom: All base-game leaders have this value set. You can leave this unchanged.
+-- SceneLayers: Similar to the above, this matches the other base-game leaders. This can be left unchanged.
+-----------------------------------------------
+
+INSERT INTO	Leaders
+			(LeaderType,			Name,							InheritFrom,		SceneLayers		)
+VALUES		('LEADER_MC_PO_NGBE',	'LOC_LEADER_MC_PO_NGBE_NAME',	'LEADER_DEFAULT', 	4				);
+
+-----------------------------------------------
+-- CivilizationLeaders
+
+-- This ties the LeaderType (defined above) to the CivilizationType (defined in Civilization_Config.sql). In this example, we're using our new, custom civilization - but any valid civilization is accepted here.
+
+-- The CapitalName field can have a different value - but typically for the first leader you are creating (alongside your custom civilization), you can reference the first CityName reference, defined in Civilization_Config.sql (specifically, the CityNames table).
+-----------------------------------------------
+
+INSERT INTO	CivilizationLeaders
+			(CivilizationType,				LeaderType,				CapitalName					)
+VALUES		('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	'LOC_CITY_NAME_RWB_NORTECHICO_1'	);
+		
+-----------------------------------------------
+-- LeaderQuotes
+
+-- This 'flavour' item ties the Quote - which appears in the Civilopedia on the Leader page - to the custom leader. As with all text entries, the LOC_PEDIA_LEADERS_PAGE_MC_PO_NGBE_QUOTE string is defined in Leader_Localisation.sql.
+-----------------------------------------------
+
+INSERT INTO	LeaderQuotes
+			(LeaderType,			Quote										)
+VALUES		('LEADER_MC_PO_NGBE',	'LOC_PEDIA_LEADERS_PAGE_MC_PO_NGBE_QUOTE'	);
+
+-----------------------------------------------
+-- CityNames
+
+-- The below section associates our locally-defined custom civilization's CityNames to the specific leader we are configuring.
+
+-- If this will be the only leader of this civilization, this isn't strictly necessary - as it will inherit from the list configured in Civilization_Config.sql.
+
+-- However, I have left this here to illustrate how variations can be achieved on a per-leader basis within a civilization.
+-----------------------------------------------
+
+INSERT INTO	CityNames
+			(CivilizationType,				LeaderType,				SortIndex,	CityName						)
+VALUES		('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_1'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_2'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_3'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_4'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_5'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_6'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_7'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_8'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_9'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_10'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_11'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_12'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_13'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_14'		),
+			('CIVILIZATION_RWB_NORTECHICO',		'LEADER_MC_PO_NGBE',	-1,			'LOC_CITY_NAME_RWB_NORTECHICO_15'		);
